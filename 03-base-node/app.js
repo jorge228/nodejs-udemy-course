35b8968d2bca19b4a1281b2b1c87f7f838f44109
jorge <jorgelozano228@gmail.com>
2021-04-11 20:02:06 +0200

    npm init and more commands

--- 03-base-node/app.js
+++ 03-base-node/app.js
@@ -1,11 +1,16 @@
 const { createTable } = require('./helpers/multiply')
 
+// npm init for package.json
+// npm run base3, from package.json
+// npm install nodemon --save-dev only appears in devDependencies
+// npm uninstall nodemon
+// npm i colors@1.0.0
+// npm update -> update all dependencies
+
 // node app --base=9
 // console.log(process.argv);
 const [, , arg3 = 'base=5'] = process.argv;
 const [, base] = arg3.split('=');
-
-
 
 // const base = 5;
 
