d9928b03d3a5adc0f384d898dc61456bbccfe812
jorge <jorgelozano228@gmail.com>
2021-04-30 15:59:59 +0200

    get users with limit and pagination

--- 08-restserver/controllers/user.controller.js
+++ 08-restserver/controllers/user.controller.js
@@ -3,15 +3,28 @@ const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
-const userGet = (req, res = response) => {
-    const { name, age, nif = 2234554, page, limit } = req.query;
+const userGet = async (req, res) => {
+
+    // const { name, age, nif = 2234554, page, limit } = req.query;
+    const { from = 0, limit = 5 } = req.query;
+    const query = { status: true };
+
+    // const users = await User.find(query)
+    //     .skip(Number(from))
+    //     .limit(Number(limit));
+    // const count = await User.countDocuments();
+
+    const [users, total] = await Promise.all([
+        User.find(query)
+            .skip(Number(from))
+            .limit(Number(limit)),
+        User.countDocuments()
+    ]);
+
     res.json({
         msg: 'get from API - controller',
-        name,
-        age,
-        nif,
-        page,
-        limit
+        total,
+        users
     });
 }
 
@@ -35,7 +48,7 @@ const userPost = async (req, res) => {
     });
 }
 
-const userPut = async (req, res) => { 
+const userPut = async (req, res) => {
     const { id } = req.params;
     const { _id, password, google, mail, ...moreBody } = req.body;
 
@@ -45,7 +58,7 @@ const userPut = async (req, res) => {
         moreBody.password = bcrypt.hashSync(password, salt);
     }
 
-    const user = await User.findByIdAndUpdate(id, moreBody);
+    await User.findByIdAndUpdate(id, moreBody);
     const userUpdate = await User.findById(id);
 
     res.status(200).json({
