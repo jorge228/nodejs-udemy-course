cb48a1155c1854062633def2d5888455b5ec3af9
jorge <jorgelozano228@gmail.com>
2021-05-01 12:46:51 +0200

    check password, mail and user status for login

--- 09-restserver/controllers/auth.controller.js
+++ 09-restserver/controllers/auth.controller.js
@@ -1,11 +1,50 @@
 // help you with typed
 const { response } = require('express');
+const bycriptjs = require('bcryptjs');
 
-const login = (req, res = response) => {
+const User = require('../models/user');
 
-    res.json({
-        msg: 'login OK'
-    });
+const login = async (req, res = response) => {
+
+    const { mail, password } = req.body;
+
+    try {
+        // verify mail exists
+        const user = await User.findOne({ mail });
+        if (!user) {
+            return res.status(400).json({
+                msg: 'Usuario o contraseña incorrecto -> correo'
+            });
+        }
+
+        // verify status user
+        if (!user.status) {
+            return res.status(400).json({
+                msg: 'Usuario o contraseña incorrecto -> status false'
+            });
+        }
+
+        // verify password
+        const validPassword = bycriptjs.compareSync(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario o contraseña incorrecto -> password'
+            });
+        }
+
+        res.json({
+            msg: 'login OK'
+        });
+
+    } catch (error) {
+
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Error'
+        });
+
+    }
+
 
 }
 
